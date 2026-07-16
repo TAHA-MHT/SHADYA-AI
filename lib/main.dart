768bb0e04d66fab43917aba8170eff3d02ef2d24
TAHA-MHT <tahadiamondben@gmail.com>
2026-07-16 18:31:47 +0100

    Remove unused firebase_ai import

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,4 @@
 import 'package:firebase_core/firebase_core.dart';
-import 'package:firebase_ai/firebase_ai.dart';
 import 'firebase_options.dart';
 import 'package:flutter/material.dart';
 import 'package:speech_to_text/speech_to_text.dart' as stt;
