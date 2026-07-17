d6fb57e7e53bd835ce7c82d4c557ae4d113a960c
TAHA-MHT <tahadiamondben@gmail.com>
2026-07-17 15:28:41 +0100

    Remove custom build directory configuration

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,18 +4,3 @@ allprojects {
         mavenCentral()
     }
 }
-
-val newBuildDir: File = rootProject.layout.buildDirectory.dir("../build").get().asFile
-rootProject.layout.buildDirectory.value(newBuildDir)
-
-subprojects {
-    val newSubprojectBuildDir: File = newBuildDir.resolve(project.name)
-    project.layout.buildDirectory.value(newSubprojectBuildDir)
-}
-subprojects {
-    project.evaluationDependsOn(":app")
-}
-
-tasks.register<Delete>("clean") {
-    delete(rootProject.layout.buildDirectory)
-}
