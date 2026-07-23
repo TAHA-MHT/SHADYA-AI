ab8645cc6d5b126f6a6c24105225a5a6fe588f81
TAHA-MHT <tahadiamondben@gmail.com>
2026-07-23 12:33:41 +0100

    Set compileSdkVersion to 36 for subprojects

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -4,3 +4,13 @@ allprojects {
         mavenCentral()
     }
 }
+
+subprojects {
+    afterEvaluate {
+        if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
+            extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
+                compileSdkVersion(36)
+            }
+        }
+    }
+}
