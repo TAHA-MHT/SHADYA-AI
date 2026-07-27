bf452c4b6eeb692084c012d30e16c30485998bf9
TAHA-MHT <tahadiamondben@gmail.com>
2026-07-27 08:49:13 +0100

    Set Java and Kotlin compatibility to version 17

--- android/build.gradle.kts
+++ android/build.gradle.kts
@@ -10,6 +10,21 @@ subprojects {
         if (project.plugins.hasPlugin("com.android.library") || project.plugins.hasPlugin("com.android.application")) {
             extensions.findByType(com.android.build.gradle.BaseExtension::class.java)?.apply {
                 compileSdkVersion(36)
+                compileOptions {
+                    sourceCompatibility = JavaVersion.VERSION_17
+                    targetCompatibility = JavaVersion.VERSION_17
+                }
+            }
+        }
+
+        tasks.withType(JavaCompile::class.java).configureEach {
+            sourceCompatibility = JavaVersion.VERSION_17.toString()
+            targetCompatibility = JavaVersion.VERSION_17.toString()
+        }
+
+        tasks.withType(org.jetbrains.kotlin.gradle.tasks.KotlinCompile::class.java).configureEach {
+            kotlinOptions {
+                jvmTarget = "17"
             }
         }
     }
